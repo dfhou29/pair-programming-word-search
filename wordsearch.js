9dad1be3016d3c7e43732301ba415d73621bce90
Difan <dfhou29@gmail.com>
2023-08-03 00:16:41 -0700

    horizontal, vertical, horizontal reversed, vertical reversed word search implemented.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,47 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const flip = function(matrix) {
+    let transposed = [];
 
-module.exports = wordSearch
+    for (let column = 0; column < matrix[0].length; column++) {
+        transposed.push([]);
+    }
+
+    for (let row of matrix) {
+        for (let transposedColumn = 0; transposedColumn < transposed.length; transposedColumn++) {
+            transposed[transposedColumn].push(row[transposedColumn]);
+        }
+    }
+
+    return transposed;
+};
+
+const wordSearch = (letters, word) => {
+    let wordLists = [];
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    horizontalJoin.forEach((element) => {
+        wordLists.push(element);
+    });
+
+    const horizontalReversedJoin = letters.map(ls => ls.reverse().join(''));
+    horizontalReversedJoin.forEach((element) => {
+        wordLists.push(element);
+    });
+
+    const verticalJoin = flip(letters).map(ls => ls.join(''));
+    verticalJoin.forEach((element) => {
+        wordLists.push(element);
+    });
+
+    const verticalReversedJoin = flip(letters).map(ls => ls.reverse().join(''));
+    verticalReversedJoin.forEach((element) => {
+        wordLists.push(element);
+    });
+
+
+    for (const l of wordLists) {
+        if (l.includes(word)) return true;
+    }
+
+    return false;
+};
+
+module.exports = wordSearch;
